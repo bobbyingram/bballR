f4132284bb76454a0c40e5ca12508ffd71b6dfe7
TambourineMan88 <ingram.bob@gmail.com>
2017-11-12 22:10:11 +0000

    remove blank cols as function

--- R/utils.R
+++ R/utils.R
@@ -9,8 +9,12 @@ year_to_season <- function(year){
   glue::glue("{year-1}-{stringr::str_sub(year, 3, 4)}")
 }
 
+remove_blank_cols <- function(dt){
+  dt[, which(colnames(dt) != "")]
+}
+
 parse_player_ids <- function(node){
-  format <- "/players/[:alpha:]"
+  format <- "/players/[:alpha:]/"
   links <- rvest::html_nodes(node, css = "a")
   i <- stringr::str_detect(links, format)
   links <- links[i]
@@ -23,3 +27,20 @@ parse_player_ids <- function(node){
     dplyr::slice(1) %>%
     dplyr::ungroup()
 }
+
+parse_team_ids <- function(node){
+  format <- "/teams/*/"
+  links <- rvest::html_nodes(node, css = "th a")
+  i <- stringr::str_detect(links, format)
+  links <- links[i]
+  ids <- rvest::html_attr(links, name = "href") %>%
+    stringr::str_replace(format, "") %>%
+    stringr::str_replace("/", "") %>%
+    tibble::tibble(FranchiseId = .,
+                   Franchise = rvest::html_text(links)) %>%
+    dplyr::group_by(.data$Franchise) %>%
+    dplyr::slice(1) %>%
+    dplyr::ungroup()
+}
+
+
