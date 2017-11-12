350a017634623892506bd593abf5159dea179304
TambourineMan88 <ingram.bob@gmail.com>
2017-11-12 21:20:37 +0000

    Ignore '.' in R CMD CHECKs

--- R/bballR.R
+++ R/bballR.R
@@ -1,3 +1,6 @@
 #' @importFrom magrittr "%>%"
 #' @importFrom rlang ".data"
 NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
