7877880eab1aea3a6d980a6db14128ffce77e983
TambourineMan88 <ingram.bob@gmail.com>
2017-11-12 21:34:28 +0000

    extract parsing player ids as function

--- R/utils.R
+++ R/utils.R
@@ -8,3 +8,18 @@ empty_as_na <- function(x){
 year_to_season <- function(year){
   glue::glue("{year-1}-{stringr::str_sub(year, 3, 4)}")
 }
+
+parse_player_ids <- function(node){
+  format <- "/players/[:alpha:]"
+  links <- rvest::html_nodes(node, css = "a")
+  i <- stringr::str_detect(links, format)
+  links <- links[i]
+  ids <- rvest::html_attr(links, name = "href") %>%
+    stringr::str_replace(format, "") %>%
+    stringr::str_replace(".html", "") %>%
+    tibble::tibble(PlayerId = .,
+                   Player = rvest::html_text(links)) %>%
+    dplyr::group_by(.data$Player) %>%
+    dplyr::slice(1) %>%
+    dplyr::ungroup()
+}
